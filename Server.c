c83cb37bfa12579cc2f0089676278ae06a298b3b
sandyljiang <sandyljiang@users.noreply.github.com>
2018-04-29 11:23:53 -0400

    Update Server.c
    I took out the random number generator and the hashmap set up so that it would be cleaner // haven't tested it cuz i don't have client

--- Server.c
+++ Server.c
@@ -1,4 +1,5 @@
-
+// Chris Fietkiewicz (chris.fietkiewicz@case.edu)
+// Based on socket example from http://www.linuxhowtos.org/C_C++/socket.htm
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,14 +9,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #define PORT_NUMBER 9383
-
-#include < stdio.h >
-
-
 
 // Helper function for error messages (not required)
 void error(const char *msg)
@@ -23,97 +18,18 @@ void error(const char *msg)
     perror(msg);
     exit(1);
 }
-// this is all stuff for the hashtable
-struct node{
-    int key;
-    int val;
-    struct node *next;
-};
-struct table{
-    int size;
-    struct node **list;
-};
-struct table *createTable(int size){
-    struct table *t = (struct table*)malloc(sizeof(struct table));
-    t->size = size;
-    t->list = (struct node**)malloc(sizeof(struct node*)*size);
-    int i;
-    for(i=0;i<size;i++)
-        t->list[i] = NULL;
-    return t;
-}
-int hashCode(struct table *t,int key){
-    if(key<0)
-        return -(key%t->size);
-    return key%t->size;
-}
-void insert(struct table *t,int key,int val){
-    int pos = hashCode(t,key);
-    struct node *list = t->list[pos];
-    struct node *newNode = (struct node*)malloc(sizeof(struct node));
-    struct node *temp = list;
-    while(temp){
-        if(temp->key==key){
-            temp->val = val;
-            return;
-        }
-        temp = temp->next;
-    }
-    newNode->key = key;
-    newNode->val = val;
-    newNode->next = list;
-    t->list[pos] = newNode;
-}
-int lookup(struct table *t,int key){
-    int pos = hashCode(t,key);
-    struct node *list = t->list[pos];
-    struct node *temp = list;
-    while(temp){
-        if(temp->key==key){
-            return temp->val;
-        }
-        temp = temp->next;
-    }
-    return -1;
-}
-
-void printRandoms(int lower, int upper, 
-                             int count)
-{
-    int i;
-    for (i = 0; i < count; i++) {
-        int num = (rand() %
-           (upper - lower + 1)) + lower;
-        printf("%d ", num);
-    }
-}
 
 int main(int argc, char *argv[])
 {
 	// Prepare for socket communication
 	int sockfd, newsockfd, portno = PORT_NUMBER;
-	char words[2000]; // words user puts in
-	char syllables[1000];//syllables
-	int count = 0;
 	socklen_t clilen;
 	char buffer[256];
-	char haiku[500];
+	int file_size;
+    FILE *received_file;
+    int remain_data = 0;
 	struct sockaddr_in serv_addr, cli_addr;
 	int n; // Number of bytes written/read
-
-	FILE *fr;
-
-    char line[1000];
-
-    fr = fopen ("syllabletxt", "r");  /* open the file for reading syllable file*/
-
-    while(fgets(line, 1000, fr) != NULL)
-    {
-    /* get a line, up to 80 chars from fr.  done if NULL */
-    sscanf  ( line );
-    }
-    fclose(fr);  /* close the file prior to exiting the routine */
-
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0) 
 		error("ERROR opening socket");
@@ -132,7 +48,7 @@ int main(int argc, char *argv[])
 	bzero(buffer, sizeof(buffer));
 
 	// Send question to client
-	sprintf(buffer, "Excluded words?");
+	sprintf(buffer, "Send the file ?");
 	n = write(newsockfd, buffer, strlen(buffer));
 	if (n < 0) error("ERROR reading from socket");
 
@@ -140,51 +56,66 @@ int main(int argc, char *argv[])
 	bzero(buffer, sizeof(buffer));
 	n = read(newsockfd, buffer, sizeof(buffer));
 	printf("Server received: %s\n", buffer); fflush(stdout); // Not required
+	received_file = fopen("server.txt", "r");
+    if (received_file == NULL)
+        {
+                fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
 
-	// Check client's response and send reply
-	if (strcmp(buffer, "Yes") == 0) {
+                exit(EXIT_FAILURE);
+        }
+
+        remain_data = file_size;
+
+        while (((len = recv(client_socket, buffer, BUFSIZ, 0)) > 0) && (remain_data > 0))
+        {
+                fwrite(buffer, sizeof(char), len, received_file);
+                remain_data -= len;
+                fprintf(stdout, "Receive %d bytes and we hope :- %d bytes\n", len, remain_data);
+        }
+
+        // closes file
+        fclose(received_file);
+
+
+        // Send question to client
+	sprintf(buffer, "Send the file of words you want to exclude?");
+	n = write(newsockfd, buffer, strlen(buffer));
+	if (n < 0) error("ERROR reading from socket");
+
+	// Receive answer from client
+	bzero(buffer, sizeof(buffer));
+	n = read(newsockfd, buffer, sizeof(buffer));
+	printf("Server received: %s\n", buffer); fflush(stdout); // Not required
+	received_file = fopen("server.txt", "r");
+    if (received_file == NULL)
+        {
+                fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
+
+                exit(EXIT_FAILURE);
+        }
+
+        remain_data = file_size;
+
+        while (((len = recv(client_socket, buffer, BUFSIZ, 0)) > 0) && (remain_data > 0))
+        {
+                fwrite(buffer, sizeof(char), len, received_file);
+                remain_data -= len;
+                fprintf(stdout, "Receive %d bytes and we hope :- %d bytes\n", len, remain_data);
+        }
+
+        // closes file
+        fclose(received_file);
+
+	// Edit this to send haiku to Buffer
+	if (strcmp(buffer, "3") == 0) {
 		bzero(buffer, sizeof(buffer));
-		sprintf(buffer, "Alright!");
+		sprintf(buffer, "Correct! The 3 steps are: create a socket, connect to it, and send a new bulb.");
+		printf("Client was correct.\n"); fflush(stdout);
 	} else {
 		bzero(buffer, sizeof(buffer));
-		sprintf(buffer, "No? Okay goodbye!");
-		close(newsockfd);
-		close(sockfd);
-		return 0;
+		sprintf(buffer, "Nope. The answer is 3 steps: create a socket, connect to it, and send a new bulb.");
+		printf("Client was NOT correct.\n"); fflush(stdout);
 	}
-
-
-// Randomly generate a number < 15. This will be the number of syllables we start with.
-    int lower = 0, upper = 15, count = 1;
- 
-    printRandoms(lower, upper, count);
- 
-
-	// Compute a haiku with a hashtable Needs to be done but how?
-	//The server will receive the text file, sanitize/standardize the input, and then add each word to a hashmap with the syllable count as the key. When requested, the algorithm for creating a haiku will be:
- 	/* 
- 	We add this word and syllable count to another hashmap.
- 	 We have a pointer that keeps track of the current syllable count. 
- 	 We increment the pointer. 
- 	 If the current syllable total + this syllable count is over 15, we decrement the pointer until we reach a syllable count that is 15 or less. 
- 	 Once we have been able to reach 15 syllables with words (or after we reach a syllable count of 1 and are unable to add new words to reach 15),
- 	  we have a hashmap containing words that sum to 15 syllables. We iterate through the hashmap and write each word to the buffer. 
- 	  We write the resulting haiku to the shared memory.  
-		If there is an included/excluded words list, the included words will be selected first. For the excluded words list, each word in that list will be added to the hashset. 
-		As each word in the source.txt is parsed and added to the hashmap, 
-		it will be checked that the word is not on the excluded list. 
-		If it is in the excluded list, it will not be added to the hashmap containing syllable counts.
-		*/
-
- 	//Note: we assume that the words in a haiku can span line breaks, as long as the number of syllables if 15. We will not be inserting hyphenation.
-
-
-	
-
-
-	printf("Haiku"); fflush(stdout); // Print's out the haiku
-
-
 	n = write(newsockfd, buffer, sizeof(buffer));
 
 	// Cleanup
